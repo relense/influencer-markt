676057b4aec154adb569f6ff507d0b237d63a32b
Miguel <miguelmofurtado@gmail.com>
2023-06-12 21:32:30 +0100

    improve layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,12 +4,10 @@ import { Footer } from "../Footer/Footer";
 
 export const Layout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen min-w-fit flex-col justify-center">
+    <main className="flex h-screen  flex-col justify-center">
       <div className="flex h-full w-full flex-col items-center">
         <Navbar />
-        <div className="flex h-full w-full flex-col items-center">
-          {props.children}
-        </div>
+        {props.children}
         <Footer />
       </div>
     </main>
