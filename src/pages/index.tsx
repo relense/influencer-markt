4b2587e458dbdc8fe05c0fe1598ac0e76f00899f
Miguel <miguelmofurtado@gmail.com>
2023-06-12 19:57:34 +0100

    Modify index to be according to design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,55 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
+import { Layout } from "../components/Layout/Layout";
+import { Button } from "../components/Button/Button";
+import { SearchBar } from "../components/SearchBar/SearchBar";
 
 const Home: NextPage = () => {
-  const user = useUser();
+  // const { data: categories } = api.categories.getAll.useQuery();
 
-  const { data } = api.users.getAllUsers.useQuery();
+  const renderBrandIcons = () => {
+    return <div>dkoadkao</div>;
+  };
+
+  const renderSectionOneTitle = () => {
+    return (
+      <>
+        <h1 className="font-playfair text-5xl">
+          Find The Right Influencer For You
+        </h1>
+        <h2 className="p-7 font-playfair text-3xl text-gray1">
+          Everyone can choose how to influence
+        </h2>
+      </>
+    );
+  };
+
+  const renderSectionTwoTitle = () => {
+    return (
+      <div className="flex flex-col items-center justify-center py-10">
+        <h1 className="font-playfair text-5xl">
+          Don&apos;t know where to choose from?
+        </h1>
+        <h2 className="p-7 font-playfair text-3xl text-white">
+          Tell us what you need and let influencers come to you instead
+        </h2>
+        <Button title="Create Offer" />
+      </div>
+    );
+  };
 
   return (
-    <>
-      <Head>
-        <title>Influencer Market</title>
-        <meta
-          name="description"
-          content="A Market to find your favorite influencers"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div>
-          {!user.isSignedIn && (
-            <SignInButton>
-              <span className="cursor-pointer text-black">Sign in</span>
-            </SignInButton>
-          )}
-          {!!user.isSignedIn && (
-            <SignOutButton>
-              <span className="cursor-pointer text-black">Sign out</span>
-            </SignOutButton>
-          )}
+    <Layout>
+      <div className="w-full text-center">
+        <div className="flex flex-col items-center py-10">
+          {renderSectionOneTitle()}
+          {renderBrandIcons()}
+          <SearchBar />
         </div>
-        <div className="">
-          {data?.map((user) => {
-            return (
-              <div className="text-black" key={user.id}>
-                {user.email}
-              </div>
-            );
-          })}
-        </div>
-      </main>
-    </>
+        <div className="bg-influencer-green">{renderSectionTwoTitle()}</div>
+      </div>
+    </Layout>
   );
 };
 
