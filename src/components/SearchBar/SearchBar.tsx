02252cdfccb3262c32ce68df6d9de249258889eb
Miguel <miguelmofurtado@gmail.com>
2023-06-12 20:38:07 +0100

    Improve searchbar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export const SearchBar = () => {
   return (
-    <div className="m-10 h-16 w-2/3 rounded-2xl bg-red-500 shadow-lg ">
-      <span>fadfs</span>
+    <div className="m-10 flex h-16 w-5/12 items-center rounded-2xl px-4 shadow-lg">
+      <input
+        placeholder="categories"
+        className="flex h-4/5 flex-1 bg-transparent text-black"
+      />
+      <div className="flex h-12 w-14 cursor-pointer items-center justify-center rounded-2xl bg-influencer">
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="fa-xl cursor-pointer px-5 text-white"
+        />
+      </div>
     </div>
   );
 };
