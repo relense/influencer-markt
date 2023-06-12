0a449131ea0fcbd7e0b9a00496177ad5431af019
Miguel <miguelmofurtado@gmail.com>
2023-06-12 19:55:38 +0100

    add fontaewsome configurations and Head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,27 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <>
+      <Head>
+        <title>Influencer Market</title>
+        <meta
+          name="description"
+          content="A Market to find your favorite influencers"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </>
   );
 };
 
