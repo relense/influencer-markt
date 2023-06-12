c9efaccbae6c98d5daf9c5f359b35e5028320dce
Miguel <miguelmofurtado@gmail.com>
2023-06-12 20:37:52 +0100

    Add Social Media icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,47 @@ import { api } from "~/utils/api";
 import { Layout } from "../components/Layout/Layout";
 import { Button } from "../components/Button/Button";
 import { SearchBar } from "../components/SearchBar/SearchBar";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   // const { data: categories } = api.categories.getAll.useQuery();
 
+  const brands = [
+    "instagram",
+    "twitter",
+    "facebook",
+    "linkedin",
+    "pinterest",
+    "twitch",
+  ];
+
   const renderBrandIcons = () => {
-    return <div>dkoadkao</div>;
+    return (
+      <div className="flex items-center">
+        {brands.map((elem) => {
+          return (
+            <div key={elem} className="px-12">
+              <Image
+                src={`/images/${elem}.png`}
+                height={68}
+                width={68}
+                alt="Instagram"
+                className="object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
   };
 
   const renderSectionOneTitle = () => {
     return (
       <>
-        <h1 className="font-playfair text-5xl">
+        <h1 className="pointer-events-none font-playfair text-5xl">
           Find The Right Influencer For You
         </h1>
-        <h2 className="p-7 font-playfair text-3xl text-gray1">
+        <h2 className="pointer-events-none p-7 font-playfair text-3xl text-gray1">
           Everyone can choose how to influence
         </h2>
       </>
@@ -28,10 +54,10 @@ const Home: NextPage = () => {
   const renderSectionTwoTitle = () => {
     return (
       <div className="flex flex-col items-center justify-center py-10">
-        <h1 className="font-playfair text-5xl">
+        <h1 className="pointer-events-none font-playfair text-5xl">
           Don&apos;t know where to choose from?
         </h1>
-        <h2 className="p-7 font-playfair text-3xl text-white">
+        <h2 className="pointer-events-none p-7 font-playfair text-3xl text-white">
           Tell us what you need and let influencers come to you instead
         </h2>
         <Button title="Create Offer" />
