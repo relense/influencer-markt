207df2d493193afcc5dc16c2110512d25898be4b
Miguel <miguelmofurtado@gmail.com>
2023-06-13 22:11:13 +0100

    Add main title func, add stepper buttons

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -7,11 +7,14 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { useState } from "react";
+import { Button } from "../../components/Button/Button";
 
 type Step = {
   step: string;
   title: string;
   subTitle: string;
+  mainTitle: string;
+  mainSubTitle: string;
 };
 
 const steps: Step[] = [
@@ -19,31 +22,53 @@ const steps: Step[] = [
     step: "Step 1",
     title: "Online Presence",
     subTitle: "Define Your Online Profile identity",
+    mainTitle: "Establish Your Online Presence",
+    mainSubTitle:
+      "Let's Establish who you are and what defines your online identity",
   },
   {
     step: "Step 2",
     title: "Social Media",
     subTitle: "Share your social media accounts",
+    mainTitle: "Connect and Showcase Your Influence",
+    mainSubTitle: "Fill in Relevant Details, Leave the Rest Optional",
   },
   {
     step: "Step 3",
     title: "Visual Portfolio",
     subTitle: "Create a portfolio related with your needs",
+    mainTitle: "Build Your Visual Portfolio",
+    mainSubTitle: "Showcase Your Best Photos on Your Profile",
   },
   {
     step: "Step 4",
     title: "Value Packs",
     subTitle: "Design Your Profitable Value Packs",
+    mainTitle: "Customized Value Packs",
+    mainSubTitle: "Select Your Platform and Craft Your Irresistible Offer",
   },
   {
     step: "",
     title: "You're All Set",
     subTitle: "Start Exploring the Exciting Opportunities Ahead!",
+    mainTitle: "Congratulations! You're All Set to Unleash Your Influence",
+    mainSubTitle:
+      "Welcome to the World of Endless Possibilities and Impactful Connections",
   },
 ];
 
 const SignUp: NextPage = () => {
   const [currentStep, setCurrentStep] = useState<number>(0);
+
+  const changeStep = (type: "next" | "previous") => {
+    if (type === "next") {
+      if (currentStep === steps.length - 1) return;
+
+      setCurrentStep(currentStep + 1);
+    } else {
+      setCurrentStep(currentStep - 1);
+    }
+  };
 
   const renderCloseButton = () => {
     return (
@@ -111,12 +136,58 @@ const SignUp: NextPage = () => {
     );
   };
 
+  const renderTitle = () => {
+    return (
+      <div className="flex flex-col items-center justify-center gap-4 font-playfair">
+        <div className="text-4xl">{steps[currentStep]?.mainTitle}</div>
+        <div className="text-xl">{steps[currentStep]?.mainSubTitle}</div>
+      </div>
+    );
+  };
+
+  const renderStepperButtons = () => {
+    return (
+      <div className="flex flex-1 items-center justify-between">
+        {currentStep > 0 && (
+          <div>
+            <Button
+              title="Previous Step"
+              level="secondary"
+              onClick={() => changeStep("previous")}
+            />
+          </div>
+        )}
+        <div className="flex flex-1 items-center justify-end">
+          <div
+            className="cursor-pointer underline"
+            onClick={() => changeStep("next")}
+          >
+            Skip Step
+          </div>
+          <Button
+            title="Next Step"
+            level="primary"
+            onClick={() => changeStep("next")}
+          />
+        </div>
+      </div>
+    );
+  };
+
   const renderStepperDesktop = () => {
     return (
-      <div className="hidden h-full w-1/4 flex-col justify-between rounded-s-2xl bg-light-red lg:flex">
-        {renderLogo()}
-
-        {renderSteps()}
+      <div className="hidden h-full lg:flex">
+        <div className="flex h-full w-1/4 flex-col justify-between rounded-s-2xl bg-light-red">
+          {renderLogo()}
+          {renderSteps()}
+        </div>
+        <div className="flex w-full flex-col p-12">
+          {renderTitle()}
+          <div className="mt-11 flex flex-1 justify-center">
+            {renderStep1()}
+          </div>
+          <div className="flex">{renderStepperButtons()}</div>
+        </div>
       </div>
     );
   };
