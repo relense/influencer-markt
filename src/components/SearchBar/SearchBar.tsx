d6fe55e354a71307b211b0851f2859e7c5fc2881
Miguel <miguelmofurtado@gmail.com>
2023-06-13 20:28:49 +0100

    Improve SearchBar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,10 +3,10 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export const SearchBar = () => {
   return (
-    <div className="m-10 flex h-16 w-8/12 items-center rounded-2xl px-4 shadow-lg">
+    <div className="m-10 flex h-16 w-11/12 items-center rounded-2xl border-[1px] border-border-white px-4 shadow-lg lg:w-8/12">
       <input
         placeholder="categories"
-        className="flex h-4/5 flex-1 bg-transparent text-black"
+        className="flex h-4/5 flex-1 bg-transparent text-black focus:outline-none"
       />
       <div className="flex h-12 w-14 cursor-pointer items-center justify-center rounded-2xl bg-influencer">
         <FontAwesomeIcon
