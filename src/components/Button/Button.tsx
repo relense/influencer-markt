6538d7e2de2f731b6a878547988348f48ffc3362
Miguel <miguelmofurtado@gmail.com>
2023-06-13 20:27:59 +0100

    Add responsive details to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = (params: { title: string }) => {
   return (
-    <div className="m-2 cursor-pointer rounded-2xl bg-influencer px-8 py-4 text-center text-white ">
+    <div className="w-11/12 cursor-pointer rounded-lg bg-influencer px-8 py-4 text-center text-white lg:mx-5 lg:w-auto lg:rounded-2xl">
       {params.title}
     </div>
   );
