7dde716819c459aeb6407c88ee6c203ae177524a
Miguel <miguelmofurtado@gmail.com>
2023-06-16 22:37:21 +0100

    create component for steps reminder

---
+++ src/components/StepsReminder/StepsReminder.tsx
@@ -0,0 +1,8 @@
+export const StepsReminder = () => {
+  return (
+    <div className="px-4 text-center text-sm">
+      <span>Update and Modify this Information at Any Time in Your</span>{" "}
+      <span className="font-extrabold">Dashboard</span>
+    </div>
+  );
+};
