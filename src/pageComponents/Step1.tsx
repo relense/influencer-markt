24711d5deca57aed4019ea7209cc4c3c3537d5a0
Miguel <miguelmofurtado@gmail.com>
2023-06-17 18:54:36 +0100

    Handle file upload for profile picture

--- src/pageComponents/Step1.tsx
+++ src/pageComponents/Step1.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faArrowUpFromBracket,
   faCamera,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { useForm } from "react-hook-form";
 
@@ -15,6 +18,7 @@ import { CustomSelect } from "../components/CustomSelect/CustomSelect";
 import { StepsReminder } from "../components/StepsReminder/StepsReminder";
 
 type ProfileData = {
+  profilePicture: string;
   displayName: string;
   role: "brand" | "individual" | "creator";
   categories: Option[];
@@ -27,12 +31,36 @@ export const Step1 = (params: {
   changeStep: (value: "next" | "previous") => void;
   categories: Category[] | undefined;
 }) => {
+  const [profilePicture, setProfilePicture] = useState<string>();
   const {
     register,
     handleSubmit,
     setValue,
     formState: { errors },
   } = useForm<ProfileData>();
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const dataURL = reader.result as string;
+        if (profilePicture !== dataURL) {
+          setProfilePicture(dataURL);
+          setValue("profilePicture", dataURL);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleRemovePicture = () => {
+    setValue("profilePicture", "");
+    setProfilePicture("");
+  };
 
   const handleRoleChange = (value: string) => {
     if (value === "brand" || value === "individual" || value === "creator")
@@ -49,10 +77,39 @@ export const Step1 = (params: {
 
   return (
     <div className="mt-2 flex flex-1 flex-col items-center gap-4 lg:mt-11 lg:overflow-y-auto">
-      <div className="flex flex-col items-center">
-        <div className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center rounded-full border-[1px] border-gray3">
-          <FontAwesomeIcon icon={faCamera} className="fa-2x text-gray3" />
-        </div>
+      <div className="relative flex flex-col items-center gap-3">
+        {!profilePicture && (
+          <input
+            type="file"
+            onChange={handleFileUpload}
+            title=""
+            className="absolute h-full w-full cursor-pointer text-[0px] opacity-0 "
+          />
+        )}
+        {!profilePicture ? (
+          <div className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center rounded-full border-[1px] border-gray3">
+            <FontAwesomeIcon icon={faCamera} className="fa-2x text-gray3" />
+          </div>
+        ) : (
+          <div className="relative flex h-24 w-24 cursor-pointer flex-col items-center justify-center rounded-full border-[1px] border-gray3">
+            <div
+              className="absolute right-[-10px] top-0 flex h-8 w-8 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green "
+              onClick={() => handleRemovePicture()}
+            >
+              <FontAwesomeIcon
+                icon={faXmark}
+                className=" fa-lg cursor-pointer text-white"
+              />
+            </div>
+            <Image
+              src={profilePicture}
+              alt="Profile Picture"
+              width={96}
+              height={96}
+              className="h-full rounded-full object-cover"
+            />
+          </div>
+        )}
         <div className="flex flex-1 items-center justify-center gap-2 text-center text-influencer sm:gap-4">
           <div className="hidden sm:flex">
             <FontAwesomeIcon icon={faArrowUpFromBracket} />
