2652336208f466de26c41d85d3b8b880cf6e5233
Miguel <miguelmofurtado@gmail.com>
2023-06-20 21:21:10 +0100

    remove middleware used for clerk

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/first-steps", "/api/(.*)"],
-});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
