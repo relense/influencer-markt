aca1811878bbf32a19553703929e12c48d3fc0bc
Miguel <miguelmofurtado@gmail.com>
2023-06-20 21:21:46 +0100

    Add modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { type ReactElement } from "react";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Modal = ({
+  children,
+  onClose,
+}: {
+  children: ReactElement;
+  onClose: () => void;
+}) => {
+  return (
+    <>
+      <div
+        className="absolute left-0 top-0 h-full w-full bg-gray1 opacity-30"
+        onClick={() => onClose()}
+      />
+
+      <div className="absolute top-10 w-11/12 rounded-lg bg-white p-4 sm:h-auto md:top-20 md:w-2/4 lg:w-2/4 2xl:w-1/4">
+        <div
+          className="absolute right-[-10px] top-[-10px] flex h-10 w-10 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green sm:right-[-10px]"
+          onClick={() => onClose()}
+        >
+          <FontAwesomeIcon
+            icon={faXmark}
+            className=" fa-lg cursor-pointer text-white"
+          />
+        </div>
+        <div className="h-[400px] overflow-y-auto xs:h-full">{children}</div>
+      </div>
+    </>
+  );
+};
+
+export { Modal };
