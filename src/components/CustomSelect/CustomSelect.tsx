cff00c3ccb430353cd2c1ccd2d880dc2e32770c6
Miguel <miguelmofurtado@gmail.com>
2023-06-23 20:56:42 +0100

    update custom select

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,8 +1,9 @@
+import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { type UseFormRegister } from "react-hook-form";
 
-import { useRef, useState } from "react";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { useOutsideClick } from "../../utils/helper";
 
 type Option = {
@@ -11,21 +12,15 @@ type Option = {
 };
 
 export const CustomSelect = (params: {
+  register: UseFormRegister<any>;
+  name: string;
   placeholder: string;
+  value: Option;
   options: Option[] | undefined;
   handleOptionSelect: (option: Option) => void;
 }) => {
   const [selectStatus, setSelectStatus] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<Option>({
-    id: -1,
-    option: "",
-  });
   const wrapperRef = useRef(null);
-
-  const onHandleClick = (selectedOption: Option) => {
-    setSelectedOption(selectedOption);
-    params.handleOptionSelect(selectedOption);
-  };
 
   useOutsideClick(() => {
     if (selectStatus === false) return;
@@ -35,32 +30,44 @@ export const CustomSelect = (params: {
 
   return (
     <div
-      ref={wrapperRef}
-      className="h-14 w-full rounded-lg border-[1px] border-gray3"
+      className="h-14 w-full"
       onClick={() => {
         setSelectStatus(!selectStatus);
       }}
     >
-      <div className="flex  items-center justify-between p-4 ">
-        <div
-          className={`overflow-hidden whitespace-nowrap ${
-            selectedOption.id === -1 ? "text-gray2" : "text-inherit"
-          }`}
-        >
-          {selectedOption.id === -1
-            ? params.placeholder
-            : selectedOption.option}
-        </div>
-
-        <FontAwesomeIcon icon={faChevronDown} className="fa-base" />
+      <div className="relative flex items-center justify-between">
+        <input
+          {...(params.register(params.name), { required: true })}
+          ref={wrapperRef}
+          id={`${params.name}1`}
+          onKeyDown={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          className="flex h-14 w-full flex-1 cursor-pointer rounded-lg border-[1px] border-gray3 bg-transparent p-4 placeholder-gray2 caret-transparent placeholder:w-11/12"
+          placeholder={params.placeholder}
+          defaultValue={params.value.option}
+          autoComplete="off"
+        />
+        {selectStatus ? (
+          <FontAwesomeIcon
+            icon={faChevronUp}
+            className="pointer-events-none absolute right-3 top-1/2 h-8 w-8 -translate-y-1/2 transform"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className="pointer-events-none absolute right-3 top-1/2 h-8 w-8 -translate-y-1/2 transform"
+          />
+        )}
       </div>
 
       {selectStatus && (
-        <div className="relative h-auto overflow-hidden rounded-lg border-[1px] border-gray3 bg-white">
+        <div className="relative z-10 h-auto overflow-hidden rounded-lg border-[1px] border-gray3 bg-white">
           {params.options &&
             params.options.map((option) => {
               let classes = "";
-              if (selectedOption.id === option.id) {
+              if (params.value.id === option.id) {
                 classes =
                   "cursor-pointer bg-influencer-green p-4 text-white hover:bg-influencer-green hover:text-white";
               } else {
@@ -70,13 +77,13 @@ export const CustomSelect = (params: {
               return (
                 <div
                   key={option.id}
-                  onClick={() => onHandleClick(option)}
+                  onClick={() => params.handleOptionSelect(option)}
                   className={classes}
                 >
                   <div className="flex items-center justify-between">
                     <div className={`w-3/4`}>{option.option}</div>
                     <div>
-                      {selectedOption.id === option.id ? (
+                      {params.value.id === option.id ? (
                         <FontAwesomeIcon
                           icon={faCircleCheck}
                           className="fa-xl"
