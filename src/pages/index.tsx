362c0d585b70e47b7ad0d64dfb61d6335fa41f87
Miguel <miguelmofurtado@gmail.com>
2023-06-23 20:56:20 +0100

    remove session fetch from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import {
   faHandHoldingDollar,
   faLock,
 } from "@fortawesome/free-solid-svg-icons";
+import { getSession, useSession } from "next-auth/react";
 
 type Offer = {
   icon: IconDefinition;
