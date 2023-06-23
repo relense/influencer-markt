6c5ec4b1959637a08934f09ca8fccde56ec6b8af
Miguel <miguelmofurtado@gmail.com>
2023-06-23 21:04:15 +0100

    Add more user api calls

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -18,21 +18,78 @@ export const usersRouter = createTRPCRouter({
     return await ctx.prisma.socialMedia.findMany();
   }),
 
-  //   getUser: protectedProcedure
-  //     .input(
-  //       z.object({
-  //         clerkId: z.string(),
-  //       })
-  //     )
-  //     .query(async ({ ctx, input }) => {
-  //       return await ctx.prisma.user.findUnique({
-  //         where: {
-  //           clerkId: input.clerkId,
-  //         },
-  //       });
-  //     }),
+  getUser: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.user.findUnique({
+      where: { id: ctx.session.user.id },
+    });
+  }),
 
-  //   // createUser: privateProcedure
-  //   //   .input(z.object({}))
-  //   //   .mutation(async ({ ctx }) => {}),
+  getProfile: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.profile.findUnique({
+      where: { userId: ctx.session.user.id },
+      include: {
+        categories: true,
+        role: true,
+      },
+    });
+  }),
+
+  updateUser: protectedProcedure
+    .input(z.object({ firstSteps: z.boolean() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.user.upsert({
+        where: { id: ctx.session.user.id },
+        update: { firstSteps: input.firstSteps },
+        create: {},
+      });
+    }),
+
+  createProfile: protectedProcedure
+    .input(
+      z.object({
+        displayName: z.string(),
+        profilePicture: z.string(),
+        role: z.object({
+          id: z.number(),
+          name: z.string(),
+        }),
+        categories: z.array(
+          z.object({
+            id: z.number(),
+            name: z.string(),
+          })
+        ),
+        about: z.string(),
+        country: z.string(),
+        city: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const profile = await ctx.prisma.profile.create({
+        data: {
+          name: input.displayName,
+          roleId: input.role.id,
+          profilePicture: input.profilePicture,
+          categories: {
+            connect: input.categories.map((category) => ({ id: category.id })),
+          },
+          about: input.about,
+          country: input.country,
+          city: input.city,
+          userId: ctx.session.user.id,
+        },
+        include: {
+          categories: true,
+        },
+      });
+
+      await ctx.prisma.user.update({
+        where: { id: ctx.session.user.id },
+        data: {
+          profile: {
+            connect: { id: profile.id },
+          },
+        },
+      });
+    }),
 });
