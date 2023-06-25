d21c03f3d2398e674862093d6c59fe1eeb0d3a3a
Miguel <miguelmofurtado@gmail.com>
2023-06-25 18:03:29 +0100

    Make modal on top of everything alwasyd

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,7 +16,7 @@ const Modal = ({
         onClick={() => onClose()}
       />
 
-      <div className="absolute top-10 w-11/12 rounded-lg bg-white p-4 sm:h-auto md:top-20 md:w-2/4 lg:w-2/4 2xl:w-1/4">
+      <div className="absolute top-10 z-40 w-11/12 rounded-lg bg-white p-4 sm:h-auto md:top-20 md:w-2/4 lg:w-2/4 2xl:w-1/4">
         <div
           className="absolute right-[-10px] top-[-10px] flex h-10 w-10 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green sm:right-[-10px]"
           onClick={() => onClose()}
@@ -26,7 +26,9 @@ const Modal = ({
             className=" fa-lg cursor-pointer text-white"
           />
         </div>
-        <div className="h-[400px] overflow-y-auto xs:h-full">{children}</div>
+        <div className="z-50 h-[400px] overflow-y-auto xs:h-full">
+          {children}
+        </div>
       </div>
     </>
   );
