9ac60c1ea349e582b2b5c28a439c0f0553fb4842
Miguel <miguelmofurtado@gmail.com>
2023-06-26 20:49:14 +0100

    improve modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -18,13 +18,10 @@ const Modal = ({
 
       <div className="absolute top-10 z-40 w-11/12 rounded-lg bg-white p-4 sm:h-auto md:top-20 md:w-2/4 lg:w-2/4 2xl:w-1/4">
         <div
-          className="absolute right-[-10px] top-[-10px] flex h-10 w-10 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green sm:right-[-10px]"
+          className="absolute right-[-10px] top-[-10px] flex h-10 w-10 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green sm:right-[-10px] sm:h-12 sm:w-12"
           onClick={() => onClose()}
         >
-          <FontAwesomeIcon
-            icon={faXmark}
-            className=" fa-lg cursor-pointer text-white"
-          />
+          <FontAwesomeIcon icon={faXmark} className="fa-lg text-white" />
         </div>
         <div className="z-50 h-[400px] overflow-y-auto xs:h-full">
           {children}
