8c0c273be739b76468b43a7398418dbf5028a0b4
Miguel <miguelmofurtado@gmail.com>
2023-06-26 18:52:54 +0100

    Add explore page initial steps

---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { Layout } from "../../components/Layout/Layout";
+
+const Explore: NextPage = () => {
+  return (
+    <Layout>
+      <div>OH YEAHHH</div>
+    </Layout>
+  );
+};
+
+export default Explore;
