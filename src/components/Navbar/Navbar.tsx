fc4c20b2ceaa07cc14a7bc3b0064f69cdd3ddfe6
Miguel <miguelmofurtado@gmail.com>
2023-06-27 19:00:03 +0100

    Update navbar to work better with mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,36 @@
-import { signIn, signOut } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { type Session } from "next-auth";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMessage,
   faBell,
-  faCircleUser,
+  faFileLines,
 } from "@fortawesome/free-regular-svg-icons";
 
 import { Button } from "../Button/Button";
-import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRightFromBracket,
+  faArrowRightToBracket,
+  faBars,
+  faChevronDown,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import Link from "next/link";
 
 export const Navbar = (params: {
   sessionData: Session | null;
   openLoginModal: () => void;
+  setIsSignUp: (isSignUp: boolean) => void;
 }) => {
   const [toggleHamburguer, setToggleHamburguer] = useState<boolean>(false);
+  const [toggleOptions, setToggleOptions] = useState<boolean>(false);
+
+  const handleJoinMarketplace = () => {
+    params.setIsSignUp(true);
+    params.openLoginModal();
+  };
 
   const leftNavBar = () => {
     return (
@@ -47,50 +60,49 @@ export const Navbar = (params: {
     );
   };
 
-  const rightNavBeforeLogin = () => {
+  const rightNavbar = () => {
     return (
-      <div className="flex flex-row items-center justify-end">
-        <span
-          className="cursor-pointer text-lg lg:p-2"
-          onClick={() => params.openLoginModal()}
-        >
-          Sign in
-        </span>
-        <Button title="Join Marketplace" level="primary" />
-      </div>
-    );
-  };
-
-  const rightNavbarAfterLogin = () => {
-    return (
-      <div className="flex flex-row items-center justify-end">
-        {params.sessionData && (
-          <span
-            className="cursor-pointer text-black"
-            onClick={() => void signOut()}
-          >
-            Sign out
-          </span>
+      <>
+        {!params.sessionData && (
+          <div className="flex flex-row items-center justify-end">
+            <span
+              className="cursor-pointer text-lg lg:p-2"
+              onClick={() => params.openLoginModal()}
+            >
+              Sign in
+            </span>
+            <Button
+              title="Join Marketplace"
+              level="primary"
+              onClick={() => handleJoinMarketplace()}
+            />
+          </div>
         )}
-        <FontAwesomeIcon
-          icon={faMessage}
-          className="fa-xl cursor-pointer px-5"
-        />
-        <FontAwesomeIcon icon={faBell} className="fa-xl cursor-pointer px-5" />
-        <div>
-          <FontAwesomeIcon
-            icon={faCircleUser}
-            className="fa-2xl cursor-pointer px-5"
-          />
-        </div>
-      </div>
+        {params.sessionData && (
+          <div className="flex flex-row items-center justify-end gap-8">
+            <FontAwesomeIcon
+              icon={faMessage}
+              className="fa-xl cursor-pointer"
+            />
+            <FontAwesomeIcon icon={faBell} className="fa-xl cursor-pointer" />
+            <div>
+              <FontAwesomeIcon
+                icon={!toggleOptions ? faChevronDown : faChevronUp}
+                className="fa-lg cursor-pointer"
+                onClick={() => setToggleOptions(!toggleOptions)}
+              />
+              {toggleOptions && optionsDropDownMenu()}
+            </div>
+          </div>
+        )}
+      </>
     );
   };
 
   const renderLogoTitle = () => {
     return (
       <Link href="/">
-        <h1 className="m-0 cursor-pointer text-left font-lobster text-2xl text-influencer lg:m-8 lg:text-4xl">
+        <h1 className="m-0 cursor-pointer text-left font-lobster text-2xl text-influencer lg:text-4xl">
           Influencer Markt
         </h1>
       </Link>
@@ -105,12 +117,12 @@ export const Navbar = (params: {
           className="fa-xl cursor-pointer"
           onClick={() => setToggleHamburguer(!toggleHamburguer)}
         />
-        {toggleHamburguer && dropDownMenu()}
+        {toggleHamburguer && hamburguerDropDownMenu()}
       </div>
     );
   };
 
-  const dropDownMenu = () => {
+  const hamburguerDropDownMenu = () => {
     return (
       <>
         <div
@@ -121,15 +133,90 @@ export const Navbar = (params: {
           className="absolute right-1 top-10 z-10 flex h-auto w-11/12 flex-col gap-4 rounded-2xl border-[1px] border-white1 bg-white p-8 shadow-lg sm:right-5 sm:w-72"
           onClick={() => setToggleHamburguer(!toggleHamburguer)}
         >
-          <div className="flex cursor-pointer items-center gap-6">
-            <FontAwesomeIcon icon={faCircleUser} className="fa-2xl" />
-            <Button title="Get Started" level="primary" />
+          {!params.sessionData && (
+            <>
+              <div className="flex cursor-pointer items-center gap-6">
+                <Button
+                  title="Join Marketplace"
+                  level="primary"
+                  onClick={() => handleJoinMarketplace()}
+                />
+              </div>
+              <div className="border-[1px] border-white1" />
+              <div
+                className="flex items-center gap-4"
+                onClick={() => params.openLoginModal()}
+              >
+                <FontAwesomeIcon
+                  icon={faArrowRightToBracket}
+                  className="fa-xl cursor-pointer"
+                />
+                <span className="cursor-pointer text-lg lg:p-2">Sign in</span>
+              </div>
+            </>
+          )}
+          {params.sessionData && (
+            <>
+              <Link href="/my-page" className="flex cursor-pointer gap-4">
+                <FontAwesomeIcon
+                  icon={faFileLines}
+                  className="fa-xl cursor-pointer"
+                />
+                My Page
+              </Link>
+              <div className="border-[1px] border-white1" />
+
+              <div
+                className="flex items-center gap-4"
+                onClick={() => void signOut()}
+              >
+                <FontAwesomeIcon
+                  icon={faArrowRightFromBracket}
+                  className="fa-xl cursor-pointer"
+                />
+                <span
+                  className="cursor-pointer text-lg"
+                  onClick={() => void signOut()}
+                >
+                  Sign out
+                </span>
+              </div>
+            </>
+          )}
+        </div>
+      </>
+    );
+  };
+
+  const optionsDropDownMenu = () => {
+    return (
+      <>
+        <div
+          className="absolute left-0 top-0 h-screen w-screen"
+          onClick={() => setToggleOptions(!toggleOptions)}
+        />
+        <div
+          className="absolute right-1 top-20 z-10 flex h-auto w-11/12 flex-col gap-4 rounded-2xl border-[1px] border-white1 bg-white p-8 shadow-lg sm:right-5 sm:w-72"
+          onClick={() => setToggleOptions(!toggleOptions)}
+        >
+          <Link href="/my-page" className="flex cursor-pointer gap-4">
+            <FontAwesomeIcon
+              icon={faFileLines}
+              className="fa-xl cursor-pointer"
+            />
+            My Page
+          </Link>
+
+          <div className="cursor-pointer border-[1px] border-white1" />
+          <div
+            className="flex items-center gap-4"
+            onClick={() => void signOut()}
+          >
+            <FontAwesomeIcon icon={faArrowRightFromBracket} className="fa-xl" />
+            <span className="text-lg" onClick={() => void signOut()}>
+              Sign out
+            </span>
           </div>
-          <div className="border-[1px] border-white1" />
-          <span className="cursor-pointer text-lg ">Home</span>
-          <span className="cursor-pointer text-lg ">Explore</span>
-          <div className="border-[1px] border-white1" />
-          <span className="cursor-pointer text-lg ">Sign in</span>
         </div>
       </>
     );
@@ -137,11 +224,10 @@ export const Navbar = (params: {
 
   const renderDesktopMenu = () => {
     return (
-      <div className="hidden h-16 w-full items-center px-6 py-12 lg:flex">
+      <div className="hidden h-16 w-full items-center px-6 py-12 lg:flex lg:gap-4 lg:px-12">
         {renderLogoTitle()}
         {leftNavBar()}
-        {!params.sessionData && rightNavBeforeLogin()}
-        {params.sessionData && rightNavbarAfterLogin()}
+        {rightNavbar()}
       </div>
     );
   };
