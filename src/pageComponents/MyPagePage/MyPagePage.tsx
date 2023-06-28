f4239fbac6ee14ea43cecde9fccb9aa3cf9b1d04
Miguel <miguelmofurtado@gmail.com>
2023-06-28 22:25:52 +0100

    Add functions to add and delete social media and update in real time

--- src/pageComponents/MyPagePage/MyPagePage.tsx
+++ src/pageComponents/MyPagePage/MyPagePage.tsx
@@ -4,10 +4,10 @@ import {
   faCamera,
   faPencil,
   faPlus,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { api } from "~/utils/api";
 
-import { Layout } from "../../components/Layout/Layout";
 import { PictureCarrosel } from "../../components/PictureCarrosel/PictureCarrosel";
 import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 import {
@@ -16,22 +16,53 @@ import {
 } from "../../components/AddSocialMediaModal/AddSocialMediaModal";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Layout } from "../../components/Layout/Layout";
 
 const MyPagePage = () => {
+  const ctx = api.useContext();
+
   const { data: platforms } = api.allRoutes.getAllSocialMedia.useQuery();
   const { data, isLoading } = api.profiles.getProfile.useQuery();
+
+  const { mutate: createUserSocialMedia } =
+    api.userSocialMedias.createUserSocialMedia.useMutation({
+      onSuccess: () => {
+        void ctx.profiles.getProfile.invalidate();
+      },
+    });
+  const { mutate: deleteUserSocialMedia } =
+    api.userSocialMedias.deleteUserSocialMedia.useMutation({
+      onSuccess: () => {
+        void ctx.profiles.getProfile.invalidate();
+      },
+    });
 
   const [isSocialMediaModalOpen, setIsSocialMediaModalOpen] =
     useState<boolean>(false);
 
-  const { control, register, handleSubmit, reset } =
-    useForm<SocialMediaDetails>({
-      defaultValues: {
-        platform: { id: -1, option: "" },
-      },
-    });
+  const {
+    control,
+    register,
+    reset,
+    handleSubmit: handleSubmitSocialMedia,
+  } = useForm<SocialMediaDetails>({
+    defaultValues: {
+      platform: { id: -1, name: "" },
+    },
+  });
 
   if (isLoading) return null;
+
+  const onAddSocialMedia = handleSubmitSocialMedia((data) => {
+    createUserSocialMedia({
+      followers: data.socialMediaFollowers,
+      handler: data.socialMediaHandler,
+      socialMedia: data.platform,
+    });
+
+    setIsSocialMediaModalOpen(false);
+    reset();
+  });
 
   const renderProfileDescription = () => {
     return (
@@ -43,7 +74,7 @@ const MyPagePage = () => {
             className="fa-lg cursor-pointer text-influencer"
           />
         </div>
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-2">
           <div className="flex h-24 w-24 cursor-pointer flex-col items-center justify-center rounded-full border-[1px] border-gray3">
             <FontAwesomeIcon icon={faCamera} className="fa-2x text-gray3" />
           </div>
@@ -52,11 +83,11 @@ const MyPagePage = () => {
             {data?.country}, {data?.city}
           </div>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
           <div className="text-2xl font-semibold">About</div>
           <div className="text-gray2">{data?.about}</div>
         </div>
-        <div className="flex flex-1 flex-col gap-4">
+        <div className="flex flex-1 flex-col gap-2">
           <div className="text-2xl font-semibold">Categories</div>
           <div className="flex flex-wrap gap-4">
             {data?.categories.map((category) => {
@@ -88,20 +119,35 @@ const MyPagePage = () => {
           </div>
         </div>
         <div className="flex flex-wrap gap-4">
-          {data?.userSocialMedia.map((socialMedia) => {
-            return (
-              <div
-                key={socialMedia.id}
-                className="flex flex-1 gap-2 rounded-2xl border-[1px] border-gray3 p-4 sm:flex-none"
-              >
-                <div className="font-semibold text-influencer">
-                  {socialMedia.socialMedia?.name}
+          {data?.userSocialMedia && data?.userSocialMedia.length > 0 ? (
+            data?.userSocialMedia.map((socialMedia) => {
+              return (
+                <div
+                  key={socialMedia.id}
+                  className="relative flex flex-1 gap-2 rounded-2xl border-[1px] border-gray3 p-4 sm:flex-none"
+                >
+                  <div className="font-semibold text-influencer">
+                    {socialMedia.socialMedia?.name}
+                  </div>
+                  <div>{socialMedia.handler}</div>
+                  <div>{socialMedia.followers}</div>
+                  <div
+                    className="absolute right-2 top-[-10px] z-10 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-influencer-green sm:right-[-8px] sm:top-[-8px]"
+                    onClick={() =>
+                      deleteUserSocialMedia({ id: socialMedia.id })
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={faXmark}
+                      className="fa-sm text-white"
+                    />
+                  </div>
                 </div>
-                <div>{socialMedia.handler}</div>
-                <div>{socialMedia.followers}</div>
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <div>No social media has been added</div>
+          )}
         </div>
       </div>
     );
@@ -111,7 +157,7 @@ const MyPagePage = () => {
     return (
       <div className="flex w-full flex-1 flex-col gap-4">
         <div className="text-2xl font-semibold">Visual Portfolio</div>
-        <div className="flex w-full flex-col items-center gap-4 self-start sm:items-start">
+        <div className="flex w-full flex-col items-center gap-4 ">
           <PictureCarrosel />
         </div>
       </div>
@@ -127,12 +173,12 @@ const MyPagePage = () => {
             <FontAwesomeIcon icon={faPlus} className="fa-sm cursor-pointer " />
           </div>
         </div>
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-4 ">
           {data?.valuePacks.map((valuePack) => {
             return (
               <div
                 key={valuePack.id}
-                className="flex flex-1 flex-col gap-4 rounded-2xl border-[1px] border-gray2 p-4"
+                className="flex w-full flex-col gap-4 rounded-2xl border-[1px] border-gray3 p-4 sm:w-2/4"
               >
                 <div className="flex justify-between gap-4">
                   <div className="text-xs font-semibold">{valuePack.title}</div>
@@ -158,7 +204,9 @@ const MyPagePage = () => {
                       <div>{valuePack.numberOfRevisions} Of Revisions</div>
                     </div>
                   </div>
-                  <div>{valuePack.valuePackPrice}€</div>
+                  <div className="font-semibold">
+                    {valuePack.valuePackPrice}€
+                  </div>
                 </div>
               </div>
             );
@@ -170,10 +218,10 @@ const MyPagePage = () => {
 
   return (
     <Layout>
-      <div className="flex flex-1 pb-10 sm:justify-center">
-        <div className="flex w-full cursor-default flex-col gap-12 px-12 sm:w-8/12">
-          <div className="flex w-full flex-1 flex-col gap-12 lg:flex-row">
-            <div className="flex flex-1 flex-col gap-12">
+      <div className="flex flex-1 justify-center pb-10">
+        <div className="flex w-full cursor-default flex-col gap-6 px-12 sm:w-8/12">
+          <div className="flex w-full flex-1 flex-col gap-6 xl:flex-row xl:gap-12">
+            <div className="flex flex-1 flex-col gap-6">
               {renderProfileDescription()}
             </div>
             {renderVisualPortfolio()}
@@ -183,16 +231,13 @@ const MyPagePage = () => {
         </div>
         {isSocialMediaModalOpen && data?.userSocialMedia && (
           <AddSocialMediaModal
-            addSocialMedia={() => console.log("add")}
+            addSocialMedia={onAddSocialMedia}
             platforms={platforms}
-            socialMediaList={data.userSocialMedia.map((socialMedia) => {
+            socialMediaList={data.userSocialMedia.map((item) => {
               return {
-                platform: {
-                  id: socialMedia.socialMedia?.id || -1,
-                  option: socialMedia.socialMedia?.name || "",
-                },
-                socialMediaHandler: socialMedia.handler,
-                socialMediaFollowers: socialMedia.followers,
+                platform: item.socialMedia || { id: -1, name: "" },
+                socialMediaHandler: item.handler,
+                socialMediaFollowers: item.followers,
               };
             })}
             control={control}
