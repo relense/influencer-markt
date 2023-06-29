f4e95e0327f941f21135bb6f143ed8376e05ab17
Miguel <miguelmofurtado@gmail.com>
2023-06-29 19:10:32 +0100

    add endpoints to getprofile without all the fk information and to update profile

--- src/server/api/routers/profiles.ts
+++ src/server/api/routers/profiles.ts
@@ -2,35 +2,6 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const profilesRouter = createTRPCRouter({
-  getProfile: protectedProcedure.query(async ({ ctx }) => {
-    return await ctx.prisma.profile.findUnique({
-      where: { userId: ctx.session.user.id },
-      include: {
-        categories: true,
-        role: true,
-        userSocialMedia: {
-          select: {
-            followers: true,
-            handler: true,
-            id: true,
-            socialMedia: true,
-          },
-        },
-        valuePacks: {
-          select: {
-            deliveryTime: true,
-            description: true,
-            id: true,
-            numberOfRevisions: true,
-            title: true,
-            valuePackPrice: true,
-            socialMedia: true,
-          },
-        },
-      },
-    });
-  }),
-
   createProfile: protectedProcedure
     .input(
       z.object({
@@ -99,6 +70,82 @@ export const profilesRouter = createTRPCRouter({
           profile: {
             connect: { id: profile.id },
           },
+        },
+      });
+    }),
+
+  getProfile: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.profile.findUnique({
+      where: { userId: ctx.session.user.id },
+      include: {
+        categories: true,
+        role: true,
+        userSocialMedia: {
+          select: {
+            followers: true,
+            handler: true,
+            id: true,
+            socialMedia: true,
+          },
+        },
+        valuePacks: {
+          select: {
+            deliveryTime: true,
+            description: true,
+            id: true,
+            numberOfRevisions: true,
+            title: true,
+            valuePackPrice: true,
+            socialMedia: true,
+          },
+        },
+      },
+    });
+  }),
+
+  getProfileWithoutIncludes: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.profile.findUnique({
+      where: { userId: ctx.session.user.id },
+      include: {
+        categories: true,
+        role: true,
+      },
+    });
+  }),
+
+  updateProfile: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        country: z.string(),
+        categories: z.array(
+          z.object({
+            id: z.number(),
+            name: z.string(),
+          })
+        ),
+        city: z.string(),
+        about: z.string(),
+        website: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.profile.update({
+        where: {
+          userId: ctx.session.user.id,
+        },
+        data: {
+          about: input.about,
+          categories: {
+            set: [],
+            connect: input.categories.map((category) => ({
+              id: category.id,
+            })),
+          },
+          city: input.city,
+          country: input.country,
+          name: input.name,
+          website: input.website,
         },
       });
     }),
