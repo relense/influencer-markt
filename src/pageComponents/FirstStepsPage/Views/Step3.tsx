cd01c37b3b1792f3e6bfa944bf7301bf4d3c8223
Miguel <miguelmofurtado@gmail.com>
2023-06-30 23:40:58 +0100

    update Step3

--- src/pageComponents/FirstStepsPage/Views/Step3.tsx
+++ src/pageComponents/FirstStepsPage/Views/Step3.tsx
@@ -5,7 +5,7 @@ export const Step3 = (params: {
   changeStep: (value: "next" | "previous") => void;
 }) => {
   return (
-    <div className="mt-2 flex flex-1 flex-col gap-4 lg:mt-11 lg:overflow-y-auto">
+    <div className="mt-2 flex w-full flex-1 flex-col gap-4 sm:items-center lg:mt-11 lg:overflow-y-auto">
       <form id="form-hook" onSubmit={() => params.changeStep("next")} />
       <PictureCarrosel />
       <StepsReminder />
