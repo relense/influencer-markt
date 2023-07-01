e3446f863432db1c94a5b3c4f33a4cc94bf4ce60
Miguel <miguelmofurtado@gmail.com>
2023-07-01 15:54:28 +0100

    create input just for valuepacks

---
+++ src/components/ValuePackInput/ValuePackInput.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useRef, useState } from "react";
+import { useOutsideClick } from "../../utils/helper";
+import { type ValuePackType } from "../../pageComponents/FirstStepsPage/Views/Step4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightRotate,
+  faCalendar,
+  faChevronDown,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { type Option } from "../CustomMultiSelect/CustomMultiSelect";
+import { ValuePack } from "../ValuePack/ValuePack";
+
+const ValuePackInput = (params: {
+  valuePack: ValuePackType | undefined;
+  platform: Option;
+  allValuePacks: ValuePackType[];
+  onChangeValuePack: (valuePack: ValuePackType) => void;
+}) => {
+  const [isValuePackModalOpen, setIsValuePackModal] = useState<boolean>(false);
+  const [availableValuePacks, setAvailableValuePacks] = useState<
+    ValuePackType[]
+  >([]);
+  const valuePackRef = useRef(null);
+
+  useOutsideClick(() => {
+    if (isValuePackModalOpen === false) return;
+
+    setIsValuePackModal(false);
+  }, valuePackRef);
+
+  useEffect(() => {
+    const currentlyAvailableValuePacks =
+      params.allValuePacks
+        ?.filter((valuePack) => {
+          return valuePack.platform?.id === params.platform.id;
+        })
+        .map((valuePack) => {
+          return {
+            id: valuePack.id,
+            deliveryTime: valuePack.deliveryTime,
+            description: valuePack.description,
+            numberOfRevisions: valuePack.numberOfRevisions,
+            platform: valuePack.platform,
+            title: valuePack.title,
+            valuePackPrice: valuePack.valuePackPrice,
+          };
+        }) || [];
+
+    setAvailableValuePacks(currentlyAvailableValuePacks);
+  }, [params.platform, params.allValuePacks]);
+
+  debugger;
+  return (
+    <div
+      className="h-40 w-full xl:h-32"
+      onClick={() => {
+        setIsValuePackModal(!isValuePackModalOpen);
+      }}
+    >
+      <div className="relative flex flex-1 cursor-pointer flex-col gap-4 py-4">
+        {params.valuePack ? (
+          <div className="px-4 text-sm font-semibold">
+            {params.valuePack.title}
+          </div>
+        ) : (
+          <div className="px-4 text-sm font-semibold">Value pack</div>
+        )}
+        <div className="relative flex flex-1 justify-between gap-6 px-4">
+          {params.valuePack ? (
+            <div className="w-96 overflow-hidden text-ellipsis whitespace-nowrap">
+              {params.valuePack.description}
+            </div>
+          ) : (
+            <div className="w-96 overflow-hidden text-ellipsis whitespace-nowrap text-gray2">
+              Please choose a value pack
+            </div>
+          )}
+          <div>
+            {isValuePackModalOpen ? (
+              <FontAwesomeIcon
+                icon={faChevronUp}
+                className="pointer-events-none absolute right-3 top-1/2 h-8 w-8 -translate-y-1/2 transform"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="pointer-events-none absolute right-3 top-1/2 h-8 w-8 -translate-y-1/2 transform"
+              />
+            )}
+          </div>
+        </div>
+        {params.valuePack && params.platform && (
+          <div className="flex flex-col items-start gap-2 px-4 text-sm font-medium text-gray2 xl:flex-row">
+            <div className="flex gap-2">
+              <FontAwesomeIcon
+                icon={faCalendar}
+                className="fa-lg cursor-pointer"
+              />
+              <div>{params.valuePack.deliveryTime} Days Delivery</div>
+            </div>
+            <div className="flex items-center gap-2">
+              <FontAwesomeIcon
+                icon={faArrowRightRotate}
+                className="fa-lg cursor-pointer"
+              />
+              <div>{params.valuePack.numberOfRevisions} Of Revisions</div>
+            </div>
+          </div>
+        )}
+      </div>
+      {isValuePackModalOpen && params.platform?.id > -1 && (
+        <div className="relative flex h-96 flex-1  flex-col gap-4 overflow-y-auto rounded-2xl border-[1px] border-gray3 bg-white p-4">
+          {availableValuePacks.map((valuePack) => {
+            return (
+              <div
+                key={valuePack.id}
+                onClick={() => {
+                  setIsValuePackModal(false);
+                  params.onChangeValuePack({
+                    id: valuePack.id,
+                    deliveryTime: valuePack.deliveryTime,
+                    description: valuePack.description,
+                    numberOfRevisions: valuePack.numberOfRevisions,
+                    platform: valuePack.platform,
+                    title: valuePack.title,
+                    valuePackPrice: valuePack.valuePackPrice,
+                  });
+                }}
+              >
+                <ValuePack
+                  deliveryTime={valuePack.deliveryTime}
+                  description={valuePack.description}
+                  numberOfRevisions={valuePack.numberOfRevisions}
+                  title={valuePack.title}
+                  valuePackPrice={valuePack.valuePackPrice}
+                  closeButton={false}
+                  selected={valuePack.id === params.valuePack?.id}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export { ValuePackInput };
