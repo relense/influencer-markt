aad83a5ffce8daa1ffcd3da68c2fb7d233000158
Miguel <miguelmofurtado@gmail.com>
2023-07-02 21:09:09 +0100

    Add loading to layout when loading user

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,6 @@
 import { useState, type PropsWithChildren } from "react";
+import { api } from "~/utils/api";
+
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { useSession } from "next-auth/react";
@@ -6,6 +8,7 @@ import { LoginModal } from "./LoginModal";
 import { BottomBar } from "./BottomBar";
 
 export const Layout = (props: PropsWithChildren) => {
+  const { data: user, isLoading } = api.users.getUser.useQuery();
   const { data: sessionData } = useSession();
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
@@ -15,25 +18,30 @@ export const Layout = (props: PropsWithChildren) => {
     setIsSignUp(false);
   };
 
-  return (
-    <>
-      <main className="flex h-screen w-full flex-1 flex-col">
-        <Navbar
-          sessionData={sessionData}
-          openLoginModal={() => setIsModalOpen(true)}
-          setIsSignUp={setIsSignUp}
-        />
-        <div className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0">
-          <div className="flex flex-1 flex-col">{props.children}</div>
-          <Footer />
-        </div>
-        <BottomBar />
-        <div className="flex justify-center">
-          {isModalOpen && (
-            <LoginModal onClose={() => onCloseModal()} isSignUp={isSignUp} />
-          )}
-        </div>
-      </main>
-    </>
-  );
+  if (isLoading) {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <>
+        <main className="flex h-screen w-full flex-1 flex-col">
+          <Navbar
+            username={user?.username || ""}
+            sessionData={sessionData}
+            openLoginModal={() => setIsModalOpen(true)}
+            setIsSignUp={setIsSignUp}
+          />
+          <div className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0">
+            <div className="flex flex-1 flex-col">{props.children}</div>
+            <Footer />
+          </div>
+          <BottomBar />
+          <div className="flex justify-center">
+            {isModalOpen && (
+              <LoginModal onClose={() => onCloseModal()} isSignUp={isSignUp} />
+            )}
+          </div>
+        </main>
+      </>
+    );
+  }
 };
