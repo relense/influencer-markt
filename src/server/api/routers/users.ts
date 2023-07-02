54d4d0e3af04e5981a0dac085ab488a11396ad66
Miguel <miguelmofurtado@gmail.com>
2023-07-02 21:10:13 +0100

    Add function to verify if username exists and to updateUser

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -8,7 +8,7 @@ export const usersRouter = createTRPCRouter({
     });
   }),
 
-  updateUser: protectedProcedure
+  updateUserFirstSteps: protectedProcedure
     .input(z.object({ firstSteps: z.boolean() }))
     .mutation(async ({ ctx, input }) => {
       return await ctx.prisma.user.upsert({
@@ -16,5 +16,41 @@ export const usersRouter = createTRPCRouter({
         update: { firstSteps: input.firstSteps },
         create: {},
       });
+    }),
+
+  updateUserIdentity: protectedProcedure
+    .input(
+      z.object({
+        role: z.object({
+          id: z.number(),
+          name: z.string(),
+        }),
+        username: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.user.upsert({
+        where: { id: ctx.session.user.id },
+        update: { roleId: input.role.id, username: input.username },
+        create: {},
+      });
+    }),
+
+  usernameExists: protectedProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ ctx, input }) => {
+      if (input.username === "" || input.username === "explore") {
+        return false;
+      }
+
+      const username = await ctx.prisma.user.findUnique({
+        where: { username: input.username },
+      });
+
+      if (username) {
+        return false;
+      } else {
+        return true;
+      }
     }),
 });
