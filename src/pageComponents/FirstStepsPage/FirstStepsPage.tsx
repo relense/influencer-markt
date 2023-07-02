a05f432752825c598a5b6dde949350c9bb5cc89b
Miguel <miguelmofurtado@gmail.com>
2023-07-02 17:09:34 +0100

    Update first steps to have step 0 where the user must decide the role and claim the page username

--- src/pageComponents/FirstStepsPage/FirstStepsPage.tsx
+++ src/pageComponents/FirstStepsPage/FirstStepsPage.tsx
@@ -13,15 +13,31 @@ import { Step5 } from "./Views/Step5";
 import { Button } from "../../components/Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { type SocialMediaDetails } from "../../components/AddSocialMediaModal/AddSocialMediaModal";
+import { type SocialMediaDetails } from "../../components/AddSocialMediaModal";
 import { type ProfileData } from "../../components/ProfileForm/ProfileForm";
+import { Step0 } from "./Views/Step0";
+import { type Option } from "../../components/CustomMultiSelect/CustomMultiSelect";
+
+enum StepsEnum {
+  OnlinePresence,
+  SocialMedia,
+  VisualPortfolio,
+  ValuePacks,
+  Final,
+}
 
 type Step = {
+  id: StepsEnum;
   step: string;
   title: string;
   subTitle: string;
   mainTitle: string;
   mainSubTitle: string;
+};
+
+export type UserIdentifyData = {
+  username: string;
+  role: Option;
 };
 
 export type SocialMediaData = {
@@ -32,8 +48,9 @@ export type ValuePacksData = {
   valuePacks: ValuePackType[];
 };
 
-const steps: Step[] = [
+const influencerSteps: Step[] = [
   {
+    id: StepsEnum.OnlinePresence,
     step: "Step 1",
     title: "Online Presence",
     subTitle: "Define Your Online Profile identity",
@@ -42,6 +59,7 @@ const steps: Step[] = [
       "Let's Establish who you are and what defines your online identity",
   },
   {
+    id: StepsEnum.SocialMedia,
     step: "Step 2",
     title: "Social Media",
     subTitle: "Share your social media accounts",
@@ -49,6 +67,7 @@ const steps: Step[] = [
     mainSubTitle: "Fill in Relevant Details, Leave the Rest Optional",
   },
   {
+    id: StepsEnum.VisualPortfolio,
     step: "Step 3",
     title: "Visual Portfolio",
     subTitle: "Create a portfolio related with your needs",
@@ -56,6 +75,7 @@ const steps: Step[] = [
     mainSubTitle: "Showcase Your Best Photos on Your Profile",
   },
   {
+    id: StepsEnum.ValuePacks,
     step: "Step 4",
     title: "Value Packs",
     subTitle: "Design Your Profitable Value Packs",
@@ -63,7 +83,74 @@ const steps: Step[] = [
     mainSubTitle: "Select Your Platform and Craft Your Irresistible Offer",
   },
   {
+    id: StepsEnum.Final,
     step: "Step 5",
+    title: "You're All Set",
+    subTitle: "Start Exploring the Exciting Opportunities Ahead!",
+    mainTitle: "Congratulations! You're All Set to Unleash Your Influence",
+    mainSubTitle:
+      "Welcome to the World of Endless Possibilities and Impactful Connections",
+  },
+];
+
+const individualSteps: Step[] = [
+  {
+    id: StepsEnum.OnlinePresence,
+    step: "Step 1",
+    title: "Online Presence",
+    subTitle: "Define Your Online Profile identity",
+    mainTitle: "Establish Your Online Presence",
+    mainSubTitle:
+      "Let's Establish who you are and what defines your online identity",
+  },
+  {
+    id: StepsEnum.SocialMedia,
+    step: "Step 2",
+    title: "Social Media",
+    subTitle: "Share your social media accounts",
+    mainTitle: "Connect and Showcase Your Influence",
+    mainSubTitle: "Fill in Relevant Details, Leave the Rest Optional",
+  },
+  {
+    id: StepsEnum.Final,
+    step: "Step 3",
+    title: "You're All Set",
+    subTitle: "Start Exploring the Exciting Opportunities Ahead!",
+    mainTitle: "Congratulations! You're All Set to Unleash Your Influence",
+    mainSubTitle:
+      "Welcome to the World of Endless Possibilities and Impactful Connections",
+  },
+];
+
+const brandSteps: Step[] = [
+  {
+    id: StepsEnum.OnlinePresence,
+    step: "Step 1",
+    title: "Online Presence",
+    subTitle: "Define Your Online Profile identity",
+    mainTitle: "Establish Your Online Presence",
+    mainSubTitle:
+      "Let's Establish who you are and what defines your online identity",
+  },
+  {
+    id: StepsEnum.SocialMedia,
+    step: "Step 2",
+    title: "Social Media",
+    subTitle: "Share your social media accounts",
+    mainTitle: "Connect and Showcase Your Influence",
+    mainSubTitle: "Fill in Relevant Details, Leave the Rest Optional",
+  },
+  {
+    id: StepsEnum.VisualPortfolio,
+    step: "Step 3",
+    title: "Visual Portfolio",
+    subTitle: "Create a portfolio related with your needs",
+    mainTitle: "Build Your Visual Portfolio",
+    mainSubTitle: "Showcase Your Best Photos on Your Profile",
+  },
+  {
+    id: StepsEnum.Final,
+    step: "Step 4",
     title: "You're All Set",
     subTitle: "Start Exploring the Exciting Opportunities Ahead!",
     mainTitle: "Congratulations! You're All Set to Unleash Your Influence",
@@ -74,7 +161,22 @@ const steps: Step[] = [
 
 const FirstStepsPage = () => {
   const mainContentRef = useRef<HTMLDivElement>(null);
-  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [isUserTypeFormComplete, setIsUserTypeFormComplete] =
+    useState<boolean>(false);
+  const [steps, setSteps] = useState<Step[]>(influencerSteps);
+  const [currentStep, setCurrentStep] = useState<Step>();
+  const [stepsCount, setStepsCount] = useState<number>(0);
+  const {
+    control: userIdentifyControl,
+    register: userIdentifyRegister,
+    watch: userIdentifyWatch,
+    handleSubmit: handleSubmitUserIdentifyData,
+  } = useForm<UserIdentifyData>({
+    defaultValues: {
+      role: { id: -1, name: "" },
+    },
+  });
+
   const {
     control,
     register,
@@ -84,7 +186,6 @@ const FirstStepsPage = () => {
     formState: { errors },
   } = useForm<ProfileData>({
     defaultValues: {
-      role: { id: -1, name: "" },
       gender: { id: -1, name: "" },
       categories: [],
       profilePicture: "",
@@ -113,6 +214,21 @@ const FirstStepsPage = () => {
     api.valuesPacks.createValuePacks.useMutation();
   const { mutate: userSocialMediaMutation } =
     api.userSocialMedias.createUserSocialMedias.useMutation();
+
+  const submitStep0 = handleSubmitUserIdentifyData((data) => {
+    setIsUserTypeFormComplete(true);
+
+    if (data.role.name === "Brand") {
+      setCurrentStep(brandSteps[0]);
+      setSteps(brandSteps);
+    } else if (data.role.name === "Individual") {
+      setCurrentStep(individualSteps[0]);
+      setSteps(individualSteps);
+    } else {
+      setCurrentStep(influencerSteps[0]);
+      setSteps(influencerSteps);
+    }
+  });
 
   const submitStep1 = handleSubmitProfileData(() => {
     changeStep("next");
@@ -146,12 +262,14 @@ const FirstStepsPage = () => {
         behavior: "smooth",
       });
     }
-    if (type === "next") {
-      if (currentStep === steps.length - 1) return;
+    if (type === "next" && currentStep) {
+      if (currentStep.id === StepsEnum.Final) return;
 
-      setCurrentStep(currentStep + 1);
+      setStepsCount(stepsCount + 1);
+      setCurrentStep(steps[stepsCount + 1]);
     } else {
-      setCurrentStep(currentStep - 1);
+      setStepsCount(stepsCount - 1);
+      setCurrentStep(steps[stepsCount - 1]);
     }
   };
 
@@ -165,7 +283,6 @@ const FirstStepsPage = () => {
         displayName: profileData.displayName,
         profilePicture: profileData.profilePicture,
         categories: profileData.categories,
-        role: { id: profileData.role.id, name: profileData.role.name },
         gender: { id: profileData.gender.id, name: profileData.gender.name },
         about: profileData.about,
         country: profileData.country,
@@ -216,31 +333,31 @@ const FirstStepsPage = () => {
             <ProgressRing
               radius={40}
               stroke={7}
-              progress={((currentStep + 1) / steps.length) * 100}
-              progressText={`${currentStep + 1}/${steps.length}`}
+              progress={((stepsCount + 1) / steps.length) * 100}
+              progressText={`${stepsCount + 1}/${steps.length}`}
               underBarColor={"influencer"}
               upperBarColor={"influencer-green"}
             />
           </div>
           <div className="hidden justify-center text-xl font-medium lg:flex lg:justify-start">
-            <div>{steps[currentStep]?.step}</div>
+            <div>{currentStep?.step}</div>
           </div>
           <div className="flex flex-col sm:gap-2">
             <div className="text-xl font-semibold sm:text-2xl lg:text-4xl">
-              {steps[currentStep]?.title}{" "}
+              {currentStep?.title}{" "}
             </div>
             <div className="hidden text-sm font-medium text-gray2 sm:flex sm:text-base lg:text-lg">
-              {steps[currentStep]?.subTitle}
+              {currentStep?.subTitle}
             </div>
             <div className="text-base font-medium text-gray2 sm:hidden lg:text-lg">
-              {steps[currentStep + 1]?.title
-                ? `Next: ${steps[currentStep + 1]?.title || ""} `
+              {steps[stepsCount + 1]?.title
+                ? `Next: ${steps[stepsCount + 1]?.title || ""} `
                 : ""}
             </div>
           </div>
           <div className="hidden flex-wrap justify-center gap-3 py-2 sm:flex sm:justify-normal lg:pt-4">
             {steps.map((step, index) => {
-              if (index <= currentStep) {
+              if (index <= stepsCount) {
                 return (
                   <div
                     key={step.step}
@@ -263,17 +380,11 @@ const FirstStepsPage = () => {
   };
 
   const renderStepMainTitle = () => {
-    const hideMobile =
-      currentStep < steps.length - 1
-        ? "hidden flex-col items-center justify-center gap-4 font-playfair lg:mt-12 lg:flex"
-        : "flex-col items-center text-center justify-center gap-4 font-playfair lg:mt-12 flex";
     return (
-      <div className={hideMobile}>
-        <div className="text-2xl sm:text-4xl">
-          {steps[currentStep]?.mainTitle}
-        </div>
+      <div className="flex flex-col items-center justify-center gap-4 py-4 text-center font-playfair lg:mt-12 lg:py-0">
+        <div className="text-2xl sm:text-4xl">{currentStep?.mainTitle}</div>
         <div className="text-base text-gray2 sm:text-xl">
-          {steps[currentStep]?.mainSubTitle}
+          {currentStep?.mainSubTitle}
         </div>
       </div>
     );
@@ -283,7 +394,7 @@ const FirstStepsPage = () => {
     return (
       <div className="flex-2 flex w-full flex-col justify-between gap-4 py-4 sm:flex-row sm:items-end sm:gap-0">
         <div className="flex justify-center">
-          {currentStep > 0 && (
+          {stepsCount > 0 && (
             <Button
               title="Previous Step"
               level="secondary"
@@ -292,9 +403,9 @@ const FirstStepsPage = () => {
           )}
         </div>
         <div className="flex items-center justify-center gap-4 lg:flex-row">
-          {currentStep < steps.length - 1 && (
+          {stepsCount < steps.length - 1 && (
             <>
-              {currentStep > 0 && (
+              {stepsCount > 0 && (
                 <div
                   className="hidden cursor-pointer underline lg:flex"
                   onClick={() => changeStep("next")}
@@ -307,7 +418,7 @@ const FirstStepsPage = () => {
             </>
           )}
 
-          {currentStep === steps.length - 1 && (
+          {stepsCount === steps.length - 1 && (
             <Link href="/" className="flex flex-1 justify-center sm:hidden">
               <Button
                 title="Get Started"
@@ -321,10 +432,8 @@ const FirstStepsPage = () => {
     );
   };
 
-  return (
-    <>
-      {renderCloseButton()}
-
+  const renderFirstSteps = () => {
+    return (
       <main className="h-full w-full bg-shadow-gray p-6 lg:p-8">
         <div className="flex h-full w-full flex-col rounded-2xl bg-white lg:flex-row lg:overscroll-none">
           {renderSteps()}
@@ -333,8 +442,8 @@ const FirstStepsPage = () => {
             ref={mainContentRef}
             className="flex h-full w-full flex-col overflow-y-auto sm:px-8 lg:overscroll-none"
           >
-            {currentStep < steps.length - 1 && renderStepMainTitle()}
-            {currentStep === 0 && (
+            {currentStep?.id !== StepsEnum.Final && renderStepMainTitle()}
+            {currentStep?.id === StepsEnum.OnlinePresence && (
               <Step1
                 control={control}
                 register={register}
@@ -343,7 +452,7 @@ const FirstStepsPage = () => {
                 errors={errors}
               />
             )}
-            {currentStep === 1 && (
+            {currentStep?.id === StepsEnum.SocialMedia && (
               <Step2
                 registerSocialMedia={registerSocialMedia}
                 platforms={platforms}
@@ -352,8 +461,10 @@ const FirstStepsPage = () => {
                 submit={submitStep2}
               />
             )}
-            {currentStep === 2 && <Step3 changeStep={changeStep} />}
-            {currentStep === 3 && (
+            {currentStep?.id === StepsEnum.VisualPortfolio && (
+              <Step3 changeStep={changeStep} />
+            )}
+            {currentStep?.id === StepsEnum.ValuePacks && (
               <Step4
                 changeStep={changeStep}
                 socialMedias={platforms}
@@ -362,7 +473,7 @@ const FirstStepsPage = () => {
                 submit={submitStep4}
               />
             )}
-            {currentStep === 4 && (
+            {currentStep?.id === StepsEnum.Final && (
               <div className="mt-6 flex h-full w-full flex-1 flex-col justify-center gap-8 p-4 sm:mt-0">
                 {renderStepMainTitle()}
                 <Step5 changeStep={changeStep} saveAllData={saveAllData} />
@@ -372,6 +483,49 @@ const FirstStepsPage = () => {
           </div>
         </div>
       </main>
+    );
+  };
+
+  const renderUserTypeForm = () => {
+    return (
+      <main className="h-full w-full bg-shadow-gray p-6 lg:p-8">
+        <div className="flex h-full w-full flex-col rounded-2xl bg-white lg:flex-row lg:overscroll-none">
+          <div
+            ref={mainContentRef}
+            className="flex h-full w-full flex-col overflow-y-auto sm:px-8 lg:overscroll-none"
+          >
+            <div className="flex flex-col items-center justify-center gap-4 py-4 text-center font-playfair lg:mt-12 lg:py-0">
+              <div className="text-2xl sm:text-4xl">Create Your Identity</div>
+              <div className="text-base text-gray2 sm:text-xl">
+                {
+                  "Welcome! Let's Get Started: Establish Your User And Claim Your Page"
+                }
+              </div>
+            </div>
+            <Step0
+              control={userIdentifyControl}
+              register={userIdentifyRegister}
+              watch={userIdentifyWatch}
+              submit={submitStep0}
+            />
+            <div className="flex-2 flex w-full flex-col justify-center gap-4 p-4 py-4 sm:flex-row sm:items-end sm:gap-0">
+              <Button
+                title="Start Your Journey"
+                level="primary"
+                form="form-user"
+                size="regular"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  };
+
+  return (
+    <>
+      {renderCloseButton()}
+      {isUserTypeFormComplete ? renderFirstSteps() : renderUserTypeForm()}
     </>
   );
 };
