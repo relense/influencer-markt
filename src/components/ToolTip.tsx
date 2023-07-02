23528dfa4e97ef464f908dad9217a7aa888442b2
Miguel <miguelmofurtado@gmail.com>
2023-07-02 21:09:35 +0100

    improve tooltip

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -9,7 +9,7 @@ const ToolTip = (params: { content: string }) => {
           icon={faCircleQuestion}
           className="fa-lg cursor-pointer text-gray2"
         />
-        <div className="invisible absolute top-3 z-10 w-auto rounded-lg border-[1px] bg-gray4 px-4 py-4 text-white opacity-90 group-hover:visible lg:w-96">
+        <div className="absolute left-[-65px] top-3 z-10 hidden w-auto rounded-lg border-[1px] bg-gray4 px-4 py-4 text-white opacity-90 group-hover:flex xs:w-72 lg:w-96">
           {params.content}
         </div>
       </div>
