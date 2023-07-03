de31108ab15d4dc95c6dabeb378449e8131fd3cd
Miguel <miguelmofurtado@gmail.com>
2023-07-03 20:30:06 +0100

    Add terms and privacy policy pages

---
+++ src/pages/privacy-policy/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { Layout } from "../../components/Layout";
+
+const PrivacyPolicy: NextPage = () => {
+  return (
+    <Layout>
+      <div>Privacy Policy</div>
+    </Layout>
+  );
+};
+
+export default PrivacyPolicy;
---
+++ src/pages/terms-conditions/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import { Layout } from "../../components/Layout";
+
+const TermsConditions: NextPage = () => {
+  return (
+    <Layout>
+      <div>Terms and Conditions</div>
+    </Layout>
+  );
+};
+
+export default TermsConditions;
