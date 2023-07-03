5c8b036acf3048465e9353eee6695c6a9381b52f
Miguel <miguelmofurtado@gmail.com>
2023-07-03 20:34:05 +0100

    update tooltip placement

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -9,7 +9,7 @@ const ToolTip = (params: { content: string }) => {
           icon={faCircleQuestion}
           className="fa-lg cursor-pointer text-gray2"
         />
-        <div className="absolute left-[-65px] top-3 z-10 hidden w-auto rounded-lg border-[1px] bg-gray4 px-4 py-4 text-white opacity-90 group-hover:flex xs:w-72 lg:w-96">
+        <div className="absolute top-3 z-10 hidden w-auto rounded-lg border-[1px] bg-gray4 px-4 py-4 text-white opacity-90 group-hover:flex xs:left-[-35px] xs:w-72 lg:w-96">
           {params.content}
         </div>
       </div>
