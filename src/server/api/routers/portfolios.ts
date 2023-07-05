78c501676a84bb0f139c7ca306ebc24470a70fe0
Miguel <miguelmofurtado@gmail.com>
2023-07-05 22:39:41 +0100

    add endpoint to create and add pictures to db

--- src/server/api/routers/portfolios.ts
+++ src/server/api/routers/portfolios.ts
@@ -1,12 +1,50 @@
 import { z } from "zod";
-import { createTRPCRouter } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const portfoliosRouter = createTRPCRouter({
-  // uploadPicture: publicProcedure
-  //   .input(
-  //     z.object({
-  //       picture: z.string(),
-  //     })
-  //   )
-  //   .query(async ({ ctx, input }) => {}),
+  createPicture: protectedProcedure
+    .input(
+      z.object({
+        url: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const profile = await ctx.prisma.profile.findUnique({
+        where: { userId: ctx.session.user.id },
+      });
+
+      if (profile) {
+        await ctx.prisma.portfolio.create({
+          data: {
+            url: input.url,
+            profile: {
+              connect: { id: profile.id },
+            },
+          },
+        });
+      }
+    }),
+
+  deletePicture: protectedProcedure
+    .input(
+      z.object({
+        pictureId: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.profile.update({
+        where: { userId: ctx.session.user.id },
+        data: {
+          portfolio: {
+            disconnect: {
+              id: input.pictureId,
+            },
+          },
+        },
+      });
+
+      await ctx.prisma.portfolio.delete({
+        where: { id: input.pictureId },
+      });
+    }),
 });
