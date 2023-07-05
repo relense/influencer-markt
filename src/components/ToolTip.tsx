9affdf5736a55eac6f428088c0d3c6825e960ff0
Miguel <miguelmofurtado@gmail.com>
2023-07-05 22:39:04 +0100

    Add optional color to Tooltip

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -1,14 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleQuestion } from "@fortawesome/free-regular-svg-icons";
 
-const ToolTip = (params: { content: string }) => {
+const ToolTip = (params: { content: string; color?: "warning" | "valid" }) => {
+  let color = "text-gray2";
+
+  if (params.color === "warning") {
+    color = "text-red-500";
+  } else if (params.color === "valid") {
+    color = "text-influencer-green";
+  }
+
+  const iconClasses = `fa-lg cursor-pointer ${color}`;
+
   return (
     <>
       <div className="group relative flex items-center">
-        <FontAwesomeIcon
-          icon={faCircleQuestion}
-          className="fa-lg cursor-pointer text-gray2"
-        />
+        <FontAwesomeIcon icon={faCircleQuestion} className={iconClasses} />
         <div className="absolute top-3 z-10 hidden w-auto rounded-lg border-[1px] bg-gray4 px-4 py-4 text-white opacity-90 group-hover:flex xs:left-[-35px] xs:w-72 lg:w-96">
           {params.content}
         </div>
