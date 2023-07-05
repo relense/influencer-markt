d5a087889ec01e57d55d54abd5a1dc89a9a208c8
Miguel <miguelmofurtado@gmail.com>
2023-07-05 22:42:48 +0100

    update step3 to send correct functions to PictureCarrosel

--- src/pageComponents/FirstStepsPage/Views/Step3.tsx
+++ src/pageComponents/FirstStepsPage/Views/Step3.tsx
@@ -1,13 +1,24 @@
 import { StepsReminder } from "../../../components/StepsReminder";
-import { PictureCarrosel } from "../../../components/PictureCarrosel";
+import {
+  type Picture,
+  PictureCarrosel,
+} from "../../../components/PictureCarrosel";
 
 export const Step3 = (params: {
   changeStep: (value: "next" | "previous") => void;
+  porttoflio: Picture[] | [];
+  addPicture?: (pictureUrl: string) => void;
+  deletePicture?: (pictureId: number) => void;
 }) => {
   return (
     <div className="mt-2 flex w-full flex-1 flex-col gap-4 sm:items-center lg:mt-11 lg:overflow-y-auto">
       <form id="form-hook" onSubmit={() => params.changeStep("next")} />
-      <PictureCarrosel visual={false} portfolio={[]} />
+      <PictureCarrosel
+        visual={false}
+        portfolio={params.porttoflio}
+        addPicture={params.addPicture}
+        deletePicture={params.deletePicture}
+      />
       <StepsReminder />
     </div>
   );
