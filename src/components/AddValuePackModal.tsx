81f0ccec9e244a50f8f6c2958a76b56f008835b8
Miguel <miguelmofurtado@gmail.com>
2023-07-06 21:05:14 +0100

    chore: Add label to valuepackmodal form

--- src/components/AddValuePackModal.tsx
+++ src/components/AddValuePackModal.tsx
@@ -25,86 +25,112 @@ const AddValuePackModal = (params: {
         onSubmit={params.onAddValuePack}
         className="flex h-full w-full flex-col items-center gap-4 p-4 sm:w-full sm:px-8"
       >
-        <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Value Pack Title</div>
+          <div className="flex w-full flex-col">
+            <input
+              {...params.register("title", { maxLength: 50 })}
+              required
+              type="text"
+              className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
+              placeholder="Provide a catchy and descriptive title for your offering"
+              autoComplete="off"
+            />
+            {params.errors.title &&
+              params.errors.title.type === "maxLength" && (
+                <div className="px-4 py-1 text-red-600">
+                  Max is 50 characters
+                </div>
+              )}
+          </div>
+        </div>
+        <div className="w-full border-[1px] border-white1" />
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Platform</div>
+          <Controller
+            name="platform"
+            control={params.control}
+            rules={{ required: true }}
+            render={({ field: { value, onChange } }) => {
+              return (
+                <CustomSelect
+                  register={params.register}
+                  name="platform"
+                  placeholder="Select the social media platform for your value pack"
+                  options={params.socialMedias}
+                  handleOptionSelect={onChange}
+                  value={value}
+                />
+              );
+            }}
+          />
+        </div>
+        <div className="w-full border-[1px] border-white1" />
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Value Pack Description</div>
+          <div className="flex w-full flex-col">
+            <textarea
+              {...params.register("description", { maxLength: 200 })}
+              required
+              className="box-border min-h-[10rem] w-full overflow-visible rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2 sm:h-48 sm:min-h-[unset]"
+              placeholder="Tell us about the unique offering in your package"
+              autoComplete="off"
+            />
+            {params.errors.description &&
+              params.errors.description.type === "maxLength" && (
+                <div className="px-4 py-1 text-red-600">
+                  Max is 200 characters
+                </div>
+              )}
+          </div>
+        </div>
+        <div className="w-full border-[1px] border-white1" />
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Delivery Time</div>
           <input
-            {...params.register("title", { maxLength: 50 })}
+            {...params.register("deliveryTime", {
+              valueAsNumber: true,
+            })}
             required
-            type="text"
+            type="number"
             className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
-            placeholder="Value Pack Title: Provide a catchy and descriptive title for your offering"
+            placeholder="E.g 4 days"
             autoComplete="off"
+            max="30"
+            min="0"
           />
-          {params.errors.title && params.errors.title.type === "maxLength" && (
-            <div className="px-4 py-1 text-red-600">Max is 50 characters</div>
-          )}
+          <div className="w-full border-[1px] border-white1" />
         </div>
-        <Controller
-          name="platform"
-          control={params.control}
-          rules={{ required: true }}
-          render={({ field: { value, onChange } }) => {
-            return (
-              <CustomSelect
-                register={params.register}
-                name="platform"
-                placeholder="Select the social media platform for your value pack"
-                options={params.socialMedias}
-                handleOptionSelect={onChange}
-                value={value}
-              />
-            );
-          }}
-        />
-        <div className="flex w-full flex-col">
-          <textarea
-            {...params.register("description", { maxLength: 200 })}
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Number of Revisions</div>
+          <input
+            {...params.register("numberOfRevisions", { valueAsNumber: true })}
             required
-            className="box-border min-h-[10rem] w-full overflow-visible rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2 sm:h-48 sm:min-h-[unset]"
-            placeholder="Value Pack Description: Tell us about the unique offering in your package"
+            type="number"
+            className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
+            placeholder="E.g 1"
             autoComplete="off"
+            max="3"
+            min="0"
           />
-          {params.errors.description &&
-            params.errors.description.type === "maxLength" && (
-              <div className="px-4 py-1 text-red-600">
-                Max is 200 characters
-              </div>
-            )}
         </div>
-        <input
-          {...params.register("deliveryTime", {
-            valueAsNumber: true,
-          })}
-          required
-          type="number"
-          className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
-          placeholder="Delivery Time E.g 4 days"
-          autoComplete="off"
-          max="30"
-          min="0"
-        />
-        <input
-          {...params.register("numberOfRevisions", { valueAsNumber: true })}
-          required
-          type="number"
-          className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
-          placeholder="Number of Revisions E.g 1"
-          autoComplete="off"
-          max="3"
-          min="0"
-        />
-        <input
-          {...params.register("valuePackPrice", {
-            valueAsNumber: true,
-            maxLength: 10,
-          })}
-          required
-          type="number"
-          className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
-          placeholder="Value Pack Price"
-          autoComplete="off"
-          max="1000000000"
-          min="0"
-        />
+        <div className="w-full border-[1px] border-white1" />
+        <div className="flex w-full flex-col gap-4">
+          <div className="text-xl font-medium">Value Pack Price</div>
+          <input
+            {...params.register("valuePackPrice", {
+              valueAsNumber: true,
+              maxLength: 10,
+            })}
+            required
+            type="number"
+            className="h-14 w-full rounded-lg border-[1px] border-gray3 p-4 placeholder-gray2"
+            placeholder="E.g 100"
+            autoComplete="off"
+            max="1000000000"
+            min="0"
+          />
+        </div>
         <div className="flex w-full justify-center">
           <Button type="submit" title="Add Value Pack" level="primary" />
         </div>
