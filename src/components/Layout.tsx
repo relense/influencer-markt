dc5481fee8b0d18e695b173e150a4f382ec63019
Miguel <miguelmofurtado@gmail.com>
2023-07-06 19:45:34 +0100

    chore: update layout component so that it scrolls to top when changing pathname

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
-import { useState, type PropsWithChildren } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState, type PropsWithChildren } from "react";
 import { api } from "~/utils/api";
 
-import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
 import { useSession } from "next-auth/react";
-import { LoginModal } from "./LoginModal";
 import { BottomBar } from "./BottomBar";
 import { type Option } from "./CustomMultiSelect";
+import { Footer } from "./Footer";
 import { LoadingSpinner } from "./LoadingSpinner";
+import { LoginModal } from "./LoginModal";
+import { Navbar } from "./Navbar";
 
 export const Layout = (props: PropsWithChildren) => {
+  const pathname = usePathname();
   const { data: sessionData, status } = useSession();
+  const scrollableContainer = useRef<HTMLDivElement>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
 
@@ -18,6 +21,12 @@ export const Layout = (props: PropsWithChildren) => {
     setIsModalOpen(false);
     setIsSignUp(false);
   };
+
+  useEffect(() => {
+    if (scrollableContainer && scrollableContainer.current) {
+      scrollableContainer.current?.scrollTo(0, 0);
+    }
+  }, [pathname]);
 
   const renderAppLayout = (username: string, role: Option) => {
     return (
@@ -30,7 +39,10 @@ export const Layout = (props: PropsWithChildren) => {
             openLoginModal={() => setIsModalOpen(true)}
             setIsSignUp={setIsSignUp}
           />
-          <div className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0">
+          <div
+            className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0"
+            ref={scrollableContainer}
+          >
             <div className="flex flex-1 flex-col">{props.children}</div>
             <Footer />
           </div>
