46903fb431b168578dabc3c7413dea8bd9254448
Miguel <miguelmofurtado@gmail.com>
2023-07-09 17:26:45 +0100

    chore: Finish translations to portuguese

--- src/pageComponents/FirstStepsPage/Views/Step5.tsx
+++ src/pageComponents/FirstStepsPage/Views/Step5.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import { Button } from "../../../components/Button";
+import { useTranslation } from "react-i18next";
 
 export const Step5 = (params: {
   changeStep: (value: "next" | "previous") => void;
   saveAllData: () => void;
 }) => {
+  const { t } = useTranslation();
+
   return (
     <div className="flex w-full items-center gap-8">
       <form id="form-hook" onSubmit={() => params.changeStep("next")} />
       <Link href="/" className="hidden flex-1 justify-center sm:flex">
         <Button
-          title="Get Started"
+          title={t("pages.firstSteps.step5.button")}
           level="primary"
           size="large"
           onClick={() => params.saveAllData()}
