e8cac860c2c2ab1eb2a892f0c278a6dc5a7c043d
Miguel <miguelmofurtado@gmail.com>
2023-07-10 21:42:02 +0100

    chore: Improve Layout Component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,6 @@ import { api } from "~/utils/api";
 
 import { useSession } from "next-auth/react";
 import { BottomBar } from "./BottomBar";
-import { type Option } from "./CustomMultiSelect";
 import { Footer } from "./Footer";
 import { LoadingSpinner } from "./LoadingSpinner";
 import { LoginModal } from "./LoginModal";
@@ -16,6 +15,7 @@ export const Layout = (props: PropsWithChildren) => {
   const scrollableContainer = useRef<HTMLDivElement>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const { data: user, isLoading } = api.users.getUser.useQuery();
 
   const onCloseModal = () => {
     setIsModalOpen(false);
@@ -28,46 +28,32 @@ export const Layout = (props: PropsWithChildren) => {
     }
   }, [pathname]);
 
-  const renderAppLayout = (username: string, role: Option) => {
-    return (
-      <>
-        <main className="flex h-screen w-full flex-1 flex-col">
-          <Navbar
-            username={username}
-            role={role}
-            sessionData={sessionData}
-            openLoginModal={() => setIsModalOpen(true)}
-            setIsSignUp={setIsSignUp}
-          />
-          <div
-            className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0"
-            ref={scrollableContainer}
-          >
-            <div className="flex flex-1 flex-col">{props.children}</div>
-            <Footer />
-          </div>
-          <BottomBar />
-          <div className="flex justify-center">
-            {isModalOpen && (
-              <LoginModal onClose={() => onCloseModal()} isSignUp={isSignUp} />
-            )}
-          </div>
-        </main>
-      </>
-    );
-  };
-
-  if (status === "authenticated") {
-    const { data: user, isLoading } = api.users.getUser.useQuery();
-    if (isLoading) {
-      return <LoadingSpinner />;
-    } else {
-      return renderAppLayout(user?.username || "", {
-        id: user?.role?.id || -1,
-        name: user?.role?.name || "",
-      });
-    }
+  if (isLoading || status === "loading") {
+    return <LoadingSpinner />;
   } else {
-    return renderAppLayout("", { id: -1, name: "" });
+    return (
+      <main className="flex h-screen w-full flex-1 flex-col">
+        <Navbar
+          username={user?.username || ""}
+          role={user?.role || { id: -1, name: "" }}
+          sessionData={sessionData}
+          openLoginModal={() => setIsModalOpen(true)}
+          setIsSignUp={setIsSignUp}
+        />
+        <div
+          className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0"
+          ref={scrollableContainer}
+        >
+          <div className="flex flex-1 flex-col">{props.children}</div>
+          <Footer />
+        </div>
+        <BottomBar />
+        <div className="flex justify-center">
+          {isModalOpen && (
+            <LoginModal onClose={() => onCloseModal()} isSignUp={isSignUp} />
+          )}
+        </div>
+      </main>
+    );
   }
 };
