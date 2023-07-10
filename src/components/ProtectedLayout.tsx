38b403982ac79eee924900fddb7ac198880f7f82
Miguel <miguelmofurtado@gmail.com>
2023-07-10 23:12:00 +0100

    chore: update naming

---
+++ src/components/ProtectedLayout.tsx
@@ -0,0 +1,26 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+type Props = {
+  children: React.ReactElement;
+};
+
+export const ProtectedLayout = ({ children }: Props) => {
+  const router = useRouter();
+  const { status: sessionStatus } = useSession();
+
+  useEffect(() => {
+    if (sessionStatus === "loading" || !router.isReady) return;
+
+    if (sessionStatus === "unauthenticated") {
+      void router.push({
+        pathname: "/",
+      });
+    }
+  }, [sessionStatus, router]);
+
+  if (sessionStatus === "authenticated") {
+    return children;
+  }
+};
