12c0be9b52ce88e7c16167eaf5bae1e458e8f8a9
Miguel <miguelmofurtado@gmail.com>
2023-08-19 20:47:01 +0100

    chore: update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,18 @@
+import { useTranslation } from "react-i18next";
+import { Layout } from "../components/Layout";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      {() => (
+        <div className="flex w-full flex-1 cursor-default flex-col items-center justify-center gap-6 self-center px-4 pb-10 sm:px-12 xl:w-3/4 2xl:w-3/4 3xl:w-2/4">
+          <div className="text-9xl font-bold text-influencer">404</div>
+          <div className="text-3xl">{t("pages.404.title")}</div>
+          <div className="text-3xl">{t("pages.404.subtitle")}</div>
+        </div>
+      )}
+    </Layout>
+  );
 }
