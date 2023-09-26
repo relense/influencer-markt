c3b0292fc8f80bb65a7b0a6dc059b1e64eecccfa
Miguel <miguelmofurtado@gmail.com>
2023-09-26 21:26:26 +0100

    chore: update modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -39,18 +39,18 @@ const Modal = ({
   return (
     <>
       <div
-        className="absolute left-0 top-0 h-full w-full bg-gray1 opacity-30"
+        className="fixed left-0 top-0 h-full w-full bg-gray1 opacity-30"
         onClick={() => (onCloseBackground ? onCloseBackground() : onClose())}
       />
 
-      <div className="absolute top-10 z-50 w-11/12 rounded-lg bg-white sm:h-auto md:top-10 md:w-3/4 lg:w-2/4 2xl:w-6/12 3xl:w-2/4 4xl:w-4/12 5xl:w-1/4">
+      <div className="fixed top-10 z-50 w-11/12 rounded-lg bg-white sm:h-auto md:top-10 md:w-3/4 lg:w-2/4 2xl:w-6/12 3xl:w-2/4 4xl:w-4/12 5xl:w-1/4">
         <div
           className="absolute right-[-5px] top-[-18px] flex h-10 w-10 cursor-pointer items-center justify-center self-end rounded-full bg-influencer-green xs:right-[-15px] xs:top-[-25px] sm:right-[-10px] sm:h-12 sm:w-12"
           onClick={() => onClose()}
         >
           <FontAwesomeIcon icon={faXmark} className="fa-xl text-white" />
         </div>
-        <div className="z-50 flex max-h-[55vh] flex-col overflow-y-auto xxs:max-h-[60vh] xs:max-h-[70vh] xl:max-h-[75vh] 3xl:max-h-[75vh]">
+        <div className="z-50 flex max-h-[55vh] flex-col overflow-y-auto xxs:max-h-[60vh] xs:max-h-[65vh] xl:max-h-[75vh] 3xl:max-h-[75vh]">
           {title && (
             <div className="flex flex-1 flex-col items-center">
               <div className="py-2 text-center">{title}</div>
