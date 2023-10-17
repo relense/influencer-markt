195cf8b2edbd97754bb969863028c490499bd6ec
Miguel <miguelmofurtado@gmail.com>
2023-10-17 12:00:09 +0100

    chore: add changes

--- src/pages/api/webhooks/index.ts
+++ src/pages/api/webhooks/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 
