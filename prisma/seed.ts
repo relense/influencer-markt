5079de7fb0c7e97f218a29c86e9bb686021de0e3
Miguel <miguelmofurtado@gmail.com>
2023-10-21 19:05:11 +0100

    chore: update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,7 +27,7 @@ type Data = {
 
 async function main() {
   const response = await fetch(
-    "https://publicdevinfmarkt.blob.core.windows.net/dev-inf-market-json-files/portugalStateCitiesV2.json"
+    "https://publicdevinfmarkt.blob.core.windows.net/dev-inf-market-json-files/portugalStateCitiesV3.json"
   );
   const data: Data = await response.json();
 
