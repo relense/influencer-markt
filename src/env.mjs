e39d10c2b951a32d3b020cdda68b9325d6c6ed6f
Miguel <miguelmofurtado@gmail.com>
2023-11-01 19:26:30 +0000

    chore: update env

--- src/env.mjs
+++ src/env.mjs
@@ -22,8 +22,6 @@ export const env = createEnv({
     ),
     NEXT_PUBLIC_BASE_URL: z.string(),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-    GITHUB_ID: z.string(),
-    GITHUB_SECRET: z.string(),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
 
@@ -65,8 +63,6 @@ export const env = createEnv({
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 
