00a9b864b7648d2c1f34669e32941e3f1f435005
Miguel <miguelmofurtado@gmail.com>
2023-11-10 07:48:01 +0000

    remove loading from temp layout

--- src/components/InConstructionFiles/TempLayout.tsx
+++ src/components/InConstructionFiles/TempLayout.tsx
@@ -1,23 +1,15 @@
 import { type ReactElement } from "react";
 import { TempNavbar } from "./TempNavbar";
-import { LoadingSpinner } from "../LoadingSpinner";
-import { useSession } from "next-auth/react";
 import { TempFooter } from "./TempFooter";
 
 export const TempLayout = (props: { children: () => ReactElement }) => {
-  const { status } = useSession();
-
-  if (status === "loading") {
-    return <LoadingSpinner />;
-  } else {
-    return (
-      <main className="lg:flex lg:h-screen lg:w-full lg:flex-1 lg:flex-col">
-        <TempNavbar />
-        <div className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0">
-          <div className="flex flex-1 flex-col pb-16">{props.children()}</div>
-          <TempFooter />
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main className="lg:flex lg:h-screen lg:w-full lg:flex-1 lg:flex-col">
+      <TempNavbar />
+      <div className="mb-12 flex w-full flex-1 flex-col overflow-y-auto sm:mb-0">
+        <div className="flex flex-1 flex-col pb-16">{props.children()}</div>
+        <TempFooter />
+      </div>
+    </main>
+  );
 };
