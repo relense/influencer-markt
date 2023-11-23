9b7c00b1a045ad09a92b02c42e0fd420151f8365
Miguel <miguelmofurtado@gmail.com>
2023-11-23 11:16:46 +0000

    chore: block prod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,55 @@ import type { GetServerSideProps, NextPage } from "next";
 
 import { HomePage } from "../pageComponents/HomePage/HomePage";
 import { Layout } from "../components/Layout";
+import { TempLayout } from "../components/InConstructionFiles/TempLayout";
+import { TempHomePage } from "../components/InConstructionFiles/TempHomePage";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Influencer Markt | The Creators Market Tailored For You</title>
-        <meta
-          name="description"
-          content="At Influencer Markt we specialize in connecting creators with brands for impactful campaigns. Unlock the potential of influencer marketing with our Marketplace"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-      </Head>
-      <Layout>
-        {({ openLoginModal, loggedInProfileId }) => (
-          <HomePage
-            openLoginModal={openLoginModal}
-            profileId={loggedInProfileId}
+  if (process.env.NEXT_PUBLIC_CURRENT_ENV === "PROD") {
+    return (
+      <>
+        <Head>
+          <title>Influencer Markt | The Creators Market Tailored For You</title>
+          <meta
+            name="description"
+            content="At Influencer Markt we specialize in connecting creators with brands for impactful campaigns. Unlock the potential of influencer marketing with our Marketplace"
           />
-        )}
-      </Layout>
-    </>
-  );
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <TempLayout>{() => <TempHomePage />}</TempLayout>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Influencer Markt | The Creators Market Tailored For You</title>
+          <meta
+            name="description"
+            content="At Influencer Markt we specialize in connecting creators with brands for impactful campaigns. Unlock the potential of influencer marketing with our Marketplace"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
+        <Layout>
+          {({ openLoginModal, loggedInProfileId }) => (
+            <HomePage
+              openLoginModal={openLoginModal}
+              profileId={loggedInProfileId}
+            />
+          )}
+        </Layout>
+      </>
+    );
+  }
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => ({
